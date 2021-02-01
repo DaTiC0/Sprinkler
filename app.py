4c1de0aca1b3161c0f1c0ae7e05b98e093c470e0
DaTi_Co <dekanozishvili@gmail.com>
2021-02-01 17:07:29 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, jsonify, abort, make_response, request, url_for
 import forecastio
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=False, threaded=True)
